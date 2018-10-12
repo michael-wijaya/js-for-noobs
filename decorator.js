f1ad44747e4ebf08e64e8dde95391bd0bdf0bbd8
michael-wijaya <michaelwijaya669@yahoo.co.id>
2018-10-12 21:13:38 +0800

    Remove everything except my name

--- decorator.js
+++ decorator.js
@@ -1,37 +1,3 @@
 my name is michael wijaya
 my major is EEE
 nice to meet you
-function debounce(fn, timeout) {
-  let _instance = null;
-  return (...args) => {
-    const callFn = () => {
-      var res = fn(...args);
-      return res;
-    };
-    if (_instance) clearTimeout(_instance);
-    _instance = setTimeout(callFn, timeout);
-  };
-}
-
-function logCurrentTime(...args) {
-  console.log(new Date().toString(), ...args);
-}
-
-function logNumber(x) {
-  logCurrentTime(x, 'This is the real one');
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const logNumberDebounced = debounce(logNumber, 3000);
-let counter = 1;
-setInterval(async () => {
-  logCurrentTime(counter);
-  if (counter < 5) {
-    counter += 1;
-    logNumberDebounced(5);
-    await sleep(1000);
-  }
-}, 1000);
